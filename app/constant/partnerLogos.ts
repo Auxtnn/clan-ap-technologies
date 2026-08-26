36d5953a219e3f090f672b6637a84420a58f7cff
Auxtnn <jonnichas@gmail.com>
2026-08-26 09:47:10 +0100

    aligned logos in home with services page

---
+++ app/constant/partnerLogos.ts
@@ -0,0 +1,25 @@
+interface Partner {
+  name: string;
+  path: string;
+}
+
+export const partners: Partner[] = [
+    { name: "Analyst", path: "/images/partners/analyst.png" },
+    { name: "Joules", path: "/images/partners/joules.png" },
+    { name: "Wayleader", path: "/images/partners/wayleader.png" },
+    { name: "Scaura", path: "/images/partners/scaura.png" },
+    { name: "Talleflow", path: "/images/partners/talleflow.png" },
+    { name: "Bittewallet", path: "/images/partners/bittewallet.png" },
+    { name: "Passes", path: "/images/partners/passes.svg" },
+    { name: "Cinelytic", path: "/images/partners/cinelytic.png" },
+    { name: "Insight", path: "/images/partners/insight.png" },
+    { name: "Steerhealth", path: "/images/partners/steer-health.png" },
+    { name: "Dashy", path: "/images/partners/dashy.png" },
+    { name: "Swovo", path: "/images/partners/swovo.png" },
+    { name: "Uprise", path: "/images/partners/uprise.png" },
+    { name: "Wholesome", path: "/images/partners/wholesome.png" },
+    { name: "Yayloh", path: "/images/partners/yayloh.png" },
+    { name: "Ll", path: "/images/partners/ll.png" },
+    { name: "Ideally", path: "/images/partners/ideally.png" },
+    { name: "Element", path: "/images/partners/element.png" },
+  ];
